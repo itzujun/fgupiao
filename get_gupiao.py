88e335748f3650a07aa4b3ab084902a33b0cf251
liuzujun <liuzujun@medlinker.com>
2021-08-18 16:57:06 +0800

    up

--- get_gupiao.py
+++ get_gupiao.py
@@ -8,17 +8,7 @@ import datetime
 
 class GuPiao():
     def __init__(self):
-        # self.date = time.strftime('%Y%m%d')
-
-        self.date = '20210812'
-        dd = datetime.datetime.strptime(self.date, "%Y%m%d")
-
-        now_time = int(time.mktime(dd.timetuple())) * 1000
-
         self.date = time.strftime('%Y%m%d')
-
-        # self.url = "http://15.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112405246752243253328_1628574993000&pn={page}&pz=100&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1"
-        # self.url = "http://78.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112406092635132097686_1628832941000&pn={page}&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1569674756885"
         self.url = "http://55.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112406092635132097686_1628574993000&pn={page}&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152"
 
     def get_data(self, page):
@@ -54,12 +44,11 @@ class GuPiao():
 if __name__ == "__main__":
     gu = GuPiao()
     li = []
-    for i in range(80):
+    for i in range(180):
         temp_li = gu.get_data(i + 1)
         li = li + temp_li
         print("正在下载 {page} 数据 ...".format(page=len(li)))
         pass
     df = pd.DataFrame(li)
-    print(df)
-    df.to_excel("./files/" + gu.date + '排名.xls', index=False)
-
+    df.to_excel("./files/" + gu.date + '排名.xlsx', index=False)
+    print("save success")
